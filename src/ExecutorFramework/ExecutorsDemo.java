4155840332e40b6695dfb16774f538027747bab2
rosudel <rosudel3@gmail.com>
2022-08-27 22:22:55 +0600

    commited

---
+++ src/ExecutorFramework/ExecutorsDemo.java
@@ -0,0 +1,39 @@
+package ExecutorFramework;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorsDemo {
+    public static void main(String[] args){
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        //System.out.println(executor.getClass().getName());
+
+        //We submit a task without setting any specific thread. Just executor wil select the thread.
+        executor.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+        });
+
+
+        //Now we are doing 10 tasks with 2 threads
+        for(int i = 0; i < 10; i++){
+            executor.submit(() -> {
+                System.out.println(Thread.currentThread().getName());
+            });
+
+
+        }
+
+        executor.shutdown();//exit the executor
+
+
+        //Best practice for above code
+        try{
+            executor.submit(() -> {
+                System.out.println(Thread.currentThread().getName());
+            });
+        }
+        finally {
+            executor.shutdown();
+        }
+    }
+}
